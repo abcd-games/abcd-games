690f736fe6177944ec032ce2dee0cf9bcbe6fd2d
Marcell <marcell.dechant@protonmail.com>
2024-02-03 16:10:38 +0100

    test: Update PlayerControllerTest to use a logged-in user

--- backend/src/main/java/com/github/abcdgames/backend/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/github/abcdgames/backend/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.github.abcdgames.backend.exception;
 
 import com.github.abcdgames.backend.appuser.AppUserAlreadyExistsException;
+import com.github.abcdgames.backend.player.exception.NoSuchPlayerException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -11,6 +12,12 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(AppUserAlreadyExistsException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public String handleAppUserAlreadyExistsException(AppUserAlreadyExistsException e) {
+        return e.getMessage();
+    }
+
+    @ExceptionHandler(NoSuchPlayerException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleNoSuchPlayerException(NoSuchPlayerException e) {
         return e.getMessage();
     }
 
