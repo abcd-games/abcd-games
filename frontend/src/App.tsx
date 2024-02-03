613da08c5b4caf1947eb66b0e3a10d8b943ec687
Marcell <marcell.dechant@protonmail.com>
2024-02-03 14:01:56 +0100

    chore: add react toastify for notifications

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,8 @@ import Layout from "./components/Layout.tsx";
 import LoginPage from "./pages/LoginPage.tsx";
 import {Route, Routes} from "react-router-dom";
 import RegisterPage from "./pages/RegisterPage.tsx";
+import {ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function App() {
     return (
@@ -11,6 +13,18 @@ export default function App() {
                 <Route path="/login" element={<LoginPage/>}/>
                 <Route path="/register" element={<RegisterPage/>}/>
             </Routes>
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="dark"
+            />
         </Layout>
     )
 }
