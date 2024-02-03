7c5cecc95e0aa590eea904db62c864987b1edd56
Marcell <marcell.dechant@protonmail.com>
2024-02-03 19:47:14 +0100

    chore: add register functionality

--- frontend/src/pages/RegisterPage.tsx
+++ frontend/src/pages/RegisterPage.tsx
@@ -1,21 +1,85 @@
-export default function RegisterPage(){
+import {AppUser, AppUserRequest} from "../types/AppUser.ts";
+import {ChangeEvent, FormEvent, useState} from "react";
+import {Navigate} from "react-router-dom";
+
+type RegisterPageProps = {
+    registerUser: (appUserRequest: AppUserRequest) => void,
+    loadingAppUser: boolean,
+    appUser: AppUser | null,
+}
+
+export default function RegisterPage(props: Readonly<RegisterPageProps>) {
+    const [appUserRequest, setAppUserRequest] = useState<AppUserRequest>({
+        username: "",
+        email: "",
+        password: "",
+    });
+
+    if (props.appUser) {
+        return <Navigate to={"/"}/>
+    }
+
+    function handleChange(event: ChangeEvent<HTMLInputElement>) {
+        const {name, value} = event.target;
+        setAppUserRequest(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+    }
+
+    function handleSubmit(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        props.registerUser(appUserRequest);
+        setAppUserRequest({
+            username: "",
+            email: "",
+            password: "",
+        });
+    }
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <h1 className="h3 mb-3 fw-normal">Please register to play games</h1>
 
             <div className="form-floating">
-                <input type="text" className="form-control" id="floatingInput" placeholder="Username"/>
+                <input
+                    type="text"
+                    className="form-control"
+                    id="floatingInput"
+                    placeholder="Username"
+                    name="username"
+                    value={appUserRequest.username}
+                    onChange={handleChange}
+                />
                 <label htmlFor="floatingInput">Username</label>
             </div>
             <div className="form-floating">
-                <input type="email" className="form-control my-2" id="floatingInput" placeholder="name@example.com"/>
+                <input
+                    type="email"
+                    className="form-control my-2"
+                    id="floatingInput"
+                    placeholder="name@example.com"
+                    name="email"
+                    value={appUserRequest.email}
+                    onChange={handleChange}
+                />
                 <label htmlFor="floatingInput">Email address</label>
             </div>
             <div className="form-floating">
-                <input type="password" className="form-control my-2" id="floatingPassword" placeholder="Password"/>
+                <input
+                    type="password"
+                    className="form-control my-2"
+                    id="floatingPassword"
+                    placeholder="Password"
+                    name="password"
+                    value={appUserRequest.password}
+                    onChange={handleChange}
+                />
                 <label htmlFor="floatingPassword">Password</label>
             </div>
-            <button className="btn btn-primary w-100 py-2 my-2" type="submit">Register now</button>
+            <button className="btn btn-primary w-100 py-2 my-2" type="submit" disabled={props.loadingAppUser}>Register
+                now
+            </button>
         </form>
     );
 }
